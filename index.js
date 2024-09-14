75abd99404829e23dba3c5080631d957cc9fc7e7
Juste KOUASSI <kjuste02@gmail.com>
2024-09-14 22:33:40 +0000

    sans option

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import { program } from "commander"
 
-// Configuration de base des commandes
+// Configuration du programme principal
 program
 	.name("myboilerplate")
 	.description("A boilerplate set for Django, Next.js, and Flutter projects")
@@ -10,31 +10,22 @@ program
 
 // Commande pour Next.js
 program
-	.command("next")
+	.command("mynext <projectName>")
 	.description("Create a Next.js boilerplate")
-	.action(() => {
-		console.log("Initializing Next.js boilerplate...")
-	})
 
 // Commande pour Django
 program
-	.command("django")
+	.command("mydjango <projectName> <appName>")
 	.description("Create a Django boilerplate")
-	.action(() => {
-		console.log("Initializing Django boilerplate...")
-	})
 
 // Commande pour Flutter
 program
-	.command("flutter")
+	.command("myflutter <projectName>")
 	.description("Create a Flutter boilerplate")
-	.action(() => {
-		console.log("Initializing Flutter boilerplate...")
-	})
 
 // Parse les arguments et affiche l'aide si aucune commande n'est spécifiée
 program.parse(process.argv)
 
 if (!process.argv.slice(2).length) {
-	program.outputHelp()
+	program.outputHelp().addHelpText("uiyf_h ")
 }
